84451f5dea2d7b4ece714c6546eaa993c002f99e
Roy Young <zhuoyuyang@gmail.com>
2013-02-21 03:10:05 +0800

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailgunExample::Application.config.secret_token = '4bb839b2159e0fbd1f11683f39d46fde3e8ae1e3db95fa8fc8eba556891c34a02c997ee1d0cab43bc5d6a2e9cda65e91b4edd115e376f700fec92689d2aa80d8'
