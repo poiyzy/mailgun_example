a103a604e5dfb05ea87d18c3713ae0e1c73f6fd3
Roy Young <zhuoyuyang@gmail.com>
2013-02-26 01:25:48 +0800

    Add Recipient Variables

--- app/models/mailgun_gateway.rb
+++ app/models/mailgun_gateway.rb
@@ -1,18 +1,15 @@
 class MailgunGateway
-  def send_batch_message(options={})
+  def send_batch_message
     RestClient.post(messaging_api_end_point,
-    from: default_sender,
-    to: delivery_filter(options[:to]),
-    subject: options[:subject],
-    html: billing_info_text(options[:body]),
+    from: "Mailgun Demo <billing-info@mailgun-demo.heroku.com>",
+    to: billing_recipients.map(&:email).join(", "),
+    subject: "Monthly Billing Info",
+    html: billing_info_text,
+    recipient_variables: recipient_variables(billing_recipients),
     ) if Rails.env.staging? || Rails.env.production?
   end
 
   private
-
-  def default_sender
-    "Mailgun Demo <billing-info@mailgun-demo.heroku.com>"
-  end
 
   def api_key
     @api_key ||= ENV["mailgun_api_key"]
@@ -22,17 +19,30 @@ class MailgunGateway
     @messaging_api_end_piont ||= "https://api:#{api_key}@api.mailgun.net/v2/#{ENV["mailgun_domain_name"]}/messages"
   end
 
-  def delivery_filter(emails)
-    Rails.env.production? ? emails : "zhuoyuyang@gmail.com"
+  def billing_recipients
+    User.where(locked: false)
   end
 
-  def billing_info_text(user)
+  def recipient_variables(recipients)
+    vars = recipients.map do |recipient|
+      "\"#{recipient.email}\": {\"name\":\"#{recipient.fullname}\"}"
+    end
+    "{#{vars.join(', ')}}"
+  end
+
+  def billing_info_text
 <<-EMAIL
 <html><body>
 
-Hi #{user.fullname},
+Hi %recipient.name%,
 
-<p>Your account created at #{user.created_at}</p>
+<p>
+Your bill for the current month is now available, please click on
+<br/>
+#{billing_url}
+<br/>
+#to see details.
+</p>
 
 </body></html>
 EMAIL
